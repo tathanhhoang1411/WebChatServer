1a2d785ef6bd9a2b1202adcc287b36d80a06f6a1
HOÀNG TẠ <tathanhhoang.work@gmail.com>
2024-01-29 01:42:48 +0700

    first commit

---
+++ WebChatServer/Hubs/ChatHub.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace WebChatServer.Hubs
+{
+    public class ChatHub:Hub
+    {
+        private static Dictionary<string, string> connectedClients = new Dictionary<string, string>();
+        //This method send notification to all clients
+        //if clients have communicate, it will call Sendmessage() method
+        //if clients have to receive notification from server, it will use ReceiveMessage() method 
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+        //everyone join chat group
+        public async Task JoinChat(string user, string message)
+        {
+            connectedClients[Context.ConnectionId] = user;
+            await Clients.Others.SendAsync("ReceiveMessage",user, message);
+        }
+        private async Task LeaveChat()
+        {
+            if (connectedClients.TryGetValue(Context.ConnectionId, out string user))
+            {
+                var message = $"{user} left the chat";
+                await Clients.Others.SendAsync("ReceiveMessage", user, message);
+            }
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            await LeaveChat();
+            await base.OnDisconnectedAsync(exception);
+
+        }
+    }
+}
---
+++ WebChatServer/Program.cs
@@ -0,0 +1,41 @@
+using WebChatServer.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddSignalR();//add signalR
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        builder =>
+        {
+            builder.WithOrigins("https://localhost:5500")
+                .AllowAnyHeader()
+                .WithMethods("GET", "POST")
+                .SetIsOriginAllowed((host) => true)
+                .AllowCredentials();
+        });
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+app.UseCors();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapHub<ChatHub>("/chatHub");
+app.Run();
