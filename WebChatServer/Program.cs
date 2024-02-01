5d17dbe71f7e3fb8f7c41511c9f30a28308d9a73
HOÀNG TẠ <tathanhhoang.work@gmail.com>
2024-02-01 13:11:21 +0700

    first

--- WebChatServer/Program.cs
+++ WebChatServer/Program.cs
@@ -26,12 +26,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 app.UseCors();
 app.MapControllerRoute(
